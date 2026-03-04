bc191613894626318770252c445f34bcee8ece2d
Ashish Kumar <50223990+imashishkr@users.noreply.github.com>
2026-03-04 22:19:02 +0530

    Add AppConstants for application configuration

---
+++ app/src/main/java/com/example/docscan/core/constants/AppConstants.kt
@@ -0,0 +1,13 @@
+package com.lensscan.core.constants
+
+object AppConstants {
+
+    const val APP_NAME = "LensScan"
+
+    const val DOCUMENT_FOLDER = "documents"
+
+    const val DATABASE_NAME = "lensscan_db"
+
+    const val SCAN_REQUEST_CODE = 1001
+
+}
